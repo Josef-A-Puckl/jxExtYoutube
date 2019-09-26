eaaa1d1ce1d540994fe70cc230593db8eacd1460
Josef A. Puckl <info@ecomstyle.de>
2019-09-26 14:44:21 +0200

    Sturktur

--- copy_this/modules/jxmods/jxextyoutube/views/admin/de/jxyoutube_lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-  include oxRegistry::get( "oxConfigFile" )->getVar( "sShopDir" ) . '/modules/jxmods/jxextyoutube/application/views/admin/de/jxyoutube_lang.php'
-?>
--- copy_this/modules/jxmods/jxextyoutube/views/admin/en/jxyoutube_lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-  include oxRegistry::get( "oxConfigFile" )->getVar( "sShopDir" ) . '/modules/jxmods/jxextyoutube/application/views/admin/en/jxyoutube_lang.php'
-?>
